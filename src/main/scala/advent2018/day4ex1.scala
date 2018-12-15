adf56f0f72ee941adf7bfbdac05dd55b7507182e
Andrew Jones <andrew@jones.ec>
2018-12-14 20:40:31 -0500

    parse day four input

--- src/main/scala/advent2018/day4ex1.scala
+++ src/main/scala/advent2018/day4ex1.scala
@@ -3,32 +3,49 @@ package advent2018
 import scala.io.Source
 import util.control.Breaks._
 
+object Shift {
+  def apply(lines: Seq[String]): Shift = {
+    this(0, Array(true))
+  }
+}
+case class Shift(guard_id: Int, minutes: Array[Boolean])
+
 object Day4Ex1 {
   val input_data =
     Source.fromURL(advent2018.Exercise2.getClass.getResource("/day4.input")).getLines
 
-  val shifts = List[String]()
+  val shifts = collection.mutable.LinkedList[String]()
 
-  val shift_start_regex = r"""\[(\d+-\d+\d+\s+\d+:\d+\)] Guard #(\d+) begins"""
-  val wake_regex = r"""\[(\d+-\d+\d+\s+\d+:\d+\)] wakes up"""
-  val sleep_regex = r"""\[(\d+-\d+\d+\s+\d+:\d+\)] falls asleep"""
+  val shift_start_regex =
+    """.(\d+-\d+-\d+\s+\d+:\d+).\s+(Guard)""".r
+  val wake_regex =
+    ".(\\d+-\\d+-\\d+\\s+\\d+:\\d+). wakes up".r
+  val sleep_regex =
+    ".(\\d+-\\d+-\\d+\\s+\\d+:\\d+). falls asleep".r
 
-  def main(args: Array[Sring]): Unit = {
+  def split_shifts: Unit = {
+    var current_shift = collection.mutable.LinkedList[String]()
+    shifts.drop(_)
+
     for(ll <- input_data)
     {
-      var current_shift = List(String)()
-
-      case shift_start_regex(dt, guard_id) =>
-        None
-      case wake_regex(dt) =>
-        None
-      case sleep_regex(dt) =>
-        None
-      case _ =>
-        throw new Exception(ll + " did not match regex!")
-
-
-
+      ll match {
+        case shift_start_regex(dt, guard_id) =>
+          if(current_shift.length > 0) { shifts :+ current_shift }
+          current_shift = collection.mutable.LinkedList[String]()
+          current_shift :+ ll
+        case wake_regex(dt) =>
+          current_shift :+ ll
+        case sleep_regex(dt) =>
+          current_shift :+ ll
+        case _ =>
+          println(s"${ll} failed to match regex!")
+          None
+      }
     }
+  }
+
+  def main(args: Array[String]): Unit = {
+    split_shifts
   }
 }
